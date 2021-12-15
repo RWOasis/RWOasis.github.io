0a5eb4b20b0872c5f87057539dbe9ed968ae4f26
RWOasis <33454524+RWOasis@users.noreply.github.com>
2021-12-15 11:59:05 +0000

    Add files via upload

---
+++ app4/event-manager.js
@@ -0,0 +1,49 @@
+/* global AFRAME */
+AFRAME.registerComponent('event-manager', {
+
+  init: function () {
+    this.bindMethods();
+
+    this.boxGeometryEl = document.querySelector('#boxGeometry');
+    this.sphereGeometryEl = document.querySelector('#sphereGeometry');
+    this.torusGeometryEl = document.querySelector('#torusGeometry');
+
+    this.boxButtonEl = document.querySelector('#boxButton');
+    this.sphereButtonEl = document.querySelector('#sphereButton');
+    this.torusButtonEl = document.querySelector('#torusButton');
+    this.darkModeButtonEl = document.querySelector('#darkModeButton');
+
+    this.buttonToGeometry = {
+      'boxButton': this.boxGeometryEl,
+      'sphereButton': this.sphereGeometryEl,
+      'torusButton': this.torusGeometryEl
+    };
+
+    this.boxButtonEl.addEventListener('click', this.onClick);
+    this.sphereButtonEl.addEventListener('click', this.onClick);
+    this.torusButtonEl.addEventListener('click', this.onClick);
+    this.darkModeButtonEl.addEventListener('click', this.onClick);
+    this.boxButtonEl.addState('pressed');
+  },
+
+  bindMethods: function () {
+    this.onClick = this.onClick.bind(this);
+  },
+
+  onClick: function (evt) {
+    var targetEl = evt.target;
+    if (targetEl === this.boxButtonEl ||
+        targetEl === this.sphereButtonEl ||
+        targetEl === this.torusButtonEl) {
+      this.boxButtonEl.removeState('pressed');
+      this.sphereButtonEl.removeState('pressed');
+      this.torusButtonEl.removeState('pressed');
+      this.boxGeometryEl.object3D.position.set(0, 0, -50);
+      this.sphereGeometryEl.object3D.position.set(0, 0, -50);
+      this.torusGeometryEl.object3D.position.set(0, 0, -50);
+      this.buttonToGeometry[targetEl.id].object3D.position.set(0, 0.1, -3);
+    }
+
+   
+  }
+});
